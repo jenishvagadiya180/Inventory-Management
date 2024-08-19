2adec9654691523c8dbcc6b10d88894ca3e184ed
jenishVagadiya <jenishvagadiya.igenerate@gmail.com>
2024-08-19 14:51:37 +0530

    hi jenish

--- helper/services.js
+++ helper/services.js
@@ -2,7 +2,7 @@ import { validationResult } from "express-validator";
 import statusCode from "./httpStatusCode.js";
 
 
-console.log("hiii")
+console.log("hiii jenish")
 class services {
   static sendSuccess = async (res, message, payload) => {
     return services.setResponse(res, statusCode.SUCCESSFUL, message, payload);
